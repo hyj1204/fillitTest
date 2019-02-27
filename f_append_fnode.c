83f3eaee46d48d642e242542f05f0d4db800a7c1
XxZhang2017 <mynewvegaan@gmail.com>
2019-02-26 16:10:22 -0800

    Add

---
+++ f_append_fnode.c
@@ -0,0 +1,137 @@
+#include "fillit.h"
+
+int ft_check_point(char **tet, int *ycnt, int *xcnt)
+{
+	int x;
+	int y;
+
+	y = -1;
+	while (tet[++y] != NULL)
+	{
+		x = -1;
+		while (tet[y][++x] != '\0')
+		{
+			if (tet[y][x] != '.')
+			{
+				if (*ycnt > y)
+					*ycnt = y;
+				// if (*xcnt > x)
+				// 	*xcnt = x;
+			}
+		}
+	}
+	if (*ycnt == 4)
+		return (-1);
+	else
+	{
+		x = -1;
+		while (tet[*ycnt][++x])
+		{
+			if (tet[*ycnt][x] != '.')
+				if (*xcnt > x)
+					*xcnt = x;
+		}
+	}
+    // printf("the origin %d, %d\n", *ycnt, *xcnt);
+	return (1);
+	// printf("%d\n", *ycnt);	
+}
+
+void    ft_get_xy(char **p, int *x, int *y, int size)
+{
+    int co_x = 4,co_y = 4;
+    int loop_x, loop_y;
+    int index;
+
+    ft_check_point(p, &co_x, &co_y);
+    // printf("coordinate is %d, %d\n", co_x, co_y);
+    loop_x = co_x;
+    index = 0;
+    for (; p[loop_x]  && index < 4; loop_x++)
+    {
+        loop_y = 0;
+        for (; p[loop_x][loop_y] && index < 4; loop_y++)
+        {
+            if (p[loop_x][loop_y] == '#')
+            {
+                x[index] = loop_x - co_x;
+                y[index] = loop_y - co_y;
+                //  printf("offset is %d, %d\n",x[index], y[index]);
+                index++;
+            }
+        }
+    }
+    x[index] = co_x;
+    y[index] = co_y;
+}
+
+t_flist *new_fnode(char **v, char sym)
+{
+    t_flist *tf = (t_flist *)malloc(sizeof(t_flist));
+    if (!tf)
+        return (NULL);
+    ft_get_xy(v, tf->x, tf->y, 4);
+    // ft_check_point(v,&(tf->x)[4], &(tf->y)[4]);
+    
+    //  printf("the origin %d, %d\n", (tf->x)[4], (tf->y)[4]);
+
+    tf->sym = sym;
+    tf->next = NULL;
+    return (tf);
+}
+
+t_flist *append_fnode(t_flist *head, t_flist *node)
+{
+    t_flist *tmp;
+
+    if (!head)
+        return (node);
+    else
+    {
+        tmp = head;
+        while (tmp->next)
+            tmp = tmp->next;
+        tmp->next = node;
+        return (head);
+    } 
+}
+
+// void    append_fnode(t_flist **fl, char sym, char **v)
+// {
+//     t_flist *tmp;
+
+//     tmp = *fl;
+//     if (fl)
+//     {
+//         if (!(*fl))
+//         {
+//             *fl = new_fnode(v, sym);
+//         }
+//         else
+//         {
+//             while (tmp->next)
+//                 tmp = tmp->next;
+//             tmp->next = new_fnode(v, sym);
+//         }
+//     }
+// }
+
+// t_flist *append_fnode(t_flist **fl, char sym)
+// {
+//     t_flist *tmp;
+
+//     tmp = *fl;
+//     if (fl)
+//     {
+//         if (!(*fl))
+//             *fl = new_fnode(p);
+//         else
+//         {
+//             while (tmp->next)
+//                 tmp = tmp->next;
+//             tmp->next = new_fnode(p);
+//         }
+//         return (*fl);
+//     }
+//     return (NULL);
+// }
---
+++ test.c
@@ -0,0 +1,163 @@
+
+
+//test get_next_line;
+// #include "fillit.h"
+
+// int main()
+// {
+//      char    *fname = "text";
+// int fd;
+//     int reg;
+//     char    *line;
+   
+//     int p_count;
+//     int     i;
+  
+    
+//     line = NULL;
+//     reg = 0;
+  
+//     fd = open(fname, O_RDONLY);
+  
+
+//     while (((reg = get_next_line(fd, &line)) != 0) )
+// 	{
+//         printf("%s", line);
+        
+//     }
+// }
+
+# include "fillit.h"
+
+// int main()
+// {
+//     // char test[4][4] =
+//     // {
+//     //     {'.', '.', '.', '.'},
+//     //     {'#', '#', '#', '.'},
+//     //     {'#', '.', '.', '.'},
+//     //     {'.', '.', '.', '.'}
+//     // };
+//     char **p;
+//     char **k;
+//     char **m;
+//     t_flist *tmp;
+//     t_flist *head;
+//     head = NULL;
+//     p = ft_create_board(4);
+//     k = ft_create_board(4);
+//     m = ft_create_board(4);
+//     p[1][0]='#';
+//     p[1][1]='#';
+//     p[0][0]='#';
+//     p[0][1]='#';
+
+//     k[3][0]='#';
+//     k[3][1]='#';
+//     k[2][2]='#';
+//     k[2][3]='#';
+
+//     m[1][2]='#';
+//     m[1][1]='#';
+//     m[1][0]='#';
+//     m[0][0]='#';
+
+//     printf("board:\n");
+//     ft_print(p);
+//     printf("\n");
+//     printf("the piece\n");
+//     ft_print(k);
+//     printf("\n");
+//     printf("the piece\n");
+//     ft_print(k);
+//     printf("\n");
+//     tmp = new_fnode(k, 'A');
+//     head = append_fnode_n(head, tmp);
+
+//     tmp = new_fnode(k, 'B');
+//     head = append_fnode_n(head, tmp);
+
+//     printf("the list\n");
+//     ft_printf_list(&head);
+//     printf("\n");
+//     ft_backtracking(p, head, 4);
+//     printf("after fill\n");
+//     ft_print(p);
+//     return (0);
+// }
+
+//test read_piece;
+// int main()
+// {
+//     char    *fname = "text";
+//     t_flist *   t_list;
+//     t_list = read_piece(fname);
+//     ft_printf_list(&t_list);
+//     return (0);
+// }
+
+int main()
+{
+    // char test[4][4] =
+    // {
+    //     {'.', '.', '.', '.'},
+    //     {'#', '#', '#', '.'},
+    //     {'#', '.', '.', '.'},
+    //     {'.', '.', '.', '.'}
+    // };
+    char **p;
+    char **k;
+    t_flist *fl;
+    p = ft_create_board(4);
+    k = ft_create_board(4);
+    p[1][0]='#';
+    p[1][1]='#';
+    p[2][1]='#';
+    p[3][1]='#';
+
+    ft_print(p);
+  
+
+    printf("append node\n");
+    fl = new_fnode(p, 'A');
+    printf("finish new node\n");
+    printf("%d\n",check_relate(fl->x, fl->y));
+
+    return (0);
+}
+
+// int main()
+// {
+//     // char test[4][4] =
+//     // {
+//     //     {'.', '.', '.', '.'},
+//     //     {'#', '#', '#', '.'},
+//     //     {'#', '.', '.', '.'},
+//     //     {'.', '.', '.', '.'}
+//     // };
+//     char **p;
+//     char **k;
+//     t_flist **fl;
+//     p = ft_create_board(4);
+//     k = ft_create_board(4);
+//     p[1][0]='#';
+//     p[2][0]='#';
+//     p[1][1]='#';
+//     p[1][2]='#';
+
+//     k[3][0]='#';
+//     k[3][1]='#';
+//     k[3][2]='#';
+//     k[3][3]='#';
+//     fl = (t_flist**)malloc(sizeof(t_flist*));
+//     *fl = NULL;
+//     printf("append node\n");
+//     append_fnode(fl, 'A', p);
+  
+    
+//     append_fnode(fl, 'B', k);
+//     printf("print list\n");
+//     ft_printf_list(fl);
+
+//     return (0);
+// }
