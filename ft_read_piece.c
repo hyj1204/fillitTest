54446493542350cccb17b68a6fccc77108001204
XxZhang2017 <mynewvegaan@gmail.com>
2019-03-03 10:53:51 -0800

    norm ft_read_piece.c

--- ft_read_piece.c
+++ ft_read_piece.c
@@ -15,29 +15,27 @@
 t_flist		*ft_read_piece(char *av)
 {
 	int		id;
-	int		byte1;
-	int		byte2;
+	int		byte[2];
 	int		fd;
-	char	buf1[21];
-	char	buf2[2];
+	char	buf[21];
 	t_flist	*head;
 
 	id = 0;
 	head = NULL;
 	if ((fd = open(av, O_RDONLY)) < 0)
 		ft_open_error();
-	byte2 = -1;
-	while ((byte1 = read(fd, buf1, 20)) > 0 || byte2 == 1)
+	byte[1] = -1;
+	while ((byte[0] = read(fd, buf, 20)) > 0 || byte[1] == 1)
 	{
-		buf1[20] = '\0';
-		if (byte1 != 20)
+		buf[20] = '\0';
+		if (byte[0] != 20)
 			ft_byte_error();
-		byte2 = read(fd, buf2, 1);
-		buf2[1] = '\0';
-		buf2[0] = '\0';
-		head = ft_get_list(head, buf1, id++ + 65);
+		head = ft_get_list(head, buf, id++ + 65);
+		byte[1] = read(fd, buf, 1);
+		buf[1] = '\0';
+		buf[0] = '\0';
 	}
-	if (byte1 == 0 && byte2 == -1)
+	if (byte[0] == 0 && byte[1] == -1)
 		ft_byte_error();
 	return (head);
 }
@@ -58,7 +56,7 @@ t_flist		*ft_get_list(t_flist *head, char buf[], char sym)
 	return (head);
 }
 
-char	**ft_to_piece(char buf[])
+char		**ft_to_piece(char buf[])
 {
 	char	**piece;
 	int		i;
